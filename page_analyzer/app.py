24e87c302db9a08af90ba34acecf43048d362259
Tim Volkov <volkov.timm@gmail.com>
2024-01-24 17:08:52 +0500

    add templates Bootstrap render page

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,4 +6,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template(
+        'main_page.html'
+    )
