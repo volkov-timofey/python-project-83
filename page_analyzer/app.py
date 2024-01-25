40c3e1ae9b3a8fec84062e83ff1cbbb947f69624
Tim Volkov <volkov.timm@gmail.com>
2024-01-25 13:25:17 +0500

    add /urls

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -9,3 +9,10 @@ def hello_world():
     return render_template(
         'main_page.html'
     )
+
+
+@app.route("/urls")
+def get_table_site():
+    return render_template(
+        'table_sites.html'
+    )
