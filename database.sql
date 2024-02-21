22cb00b86965bf9fe950b05888faaa469e76c7d8
Tim Volkov <volkov.timm@gmail.com>
2024-02-21 16:44:59 +0500

    create db

--- database.sql
+++ database.sql
@@ -1,12 +1,11 @@
 --noqa: disable=L010
-DROP TABLE IF EXISTS urls, url_checks;
-CREATE TABLE urls (
+CREATE TABLE IF NOT EXISTS urls (
 	id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	name varchar(255),
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE url_checks (
+CREATE TABLE IF NOT EXISTS url_checks (
 	id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	url_id bigint ,
 	status_code bigint,
--- page_analyzer/database.py
+++ page_analyzer/database.py
@@ -124,17 +124,14 @@ def create_tables(database_url):
             BEGIN;
         ''',
         '''
-            DROP TABLE IF EXISTS urls, url_checks;
-        ''',
-        '''
-            CREATE TABLE urls (
+            CREATE TABLE IF NOT EXISTS urls (
                 id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
                 name varchar(255),
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             );
         ''',
         '''
-            CREATE TABLE url_checks (
+            CREATE TABLE IF NOT EXISTS url_checks (
                 id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
                 url_id bigint ,
                 status_code bigint,
