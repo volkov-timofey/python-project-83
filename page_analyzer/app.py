d895c7a4a7e9ff2239e2b927ecb1257986aef750
Tim Volkov <volkov.timm@gmail.com>
2024-02-22 14:24:02 +0500

    create db

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
 app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
 
-create_tables(app.config['DATABASE_URL'])
+# create_tables(app.config['DATABASE_URL'])
 
 urls_table = DataBase(app.config['DATABASE_URL'], 'urls')
 url_checks = DataBase(app.config['DATABASE_URL'], 'url_checks')
